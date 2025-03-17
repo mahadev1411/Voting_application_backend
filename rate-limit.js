bfaccb72fa3cae64e473dfc3fde5c4313f0ff00d
mahadev1411 <mahadevd3108@gmail.com>
2025-03-17 12:50:31 +0530

    Updated rate-limit.js

--- rate-limit.js
+++ rate-limit.js
@@ -3,7 +3,7 @@ const rateLimit = require("express-rate-limit");
 // Define rate limit rule for login
 const loginLimiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 1 minutes
-    max: 2, // Allow 5 login attempts per 15 minutes
+    max: 2, // Allow 5 login attempts per 1 minutes
     message: "Too many login attempts. Please try again later.",
     headers: true,
 });
